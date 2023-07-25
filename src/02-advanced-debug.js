886b16a3177dd8e5a2ecf06e5267cd44bc0e4331
Billy <billychorey@gmail.com>
2023-07-25 16:22:15 -0400

    ballgame object exercise

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -1,24 +1,24 @@
 console.log('Advanced debugging example running.')
-debugger
+//debugger
 
 // first, define the function.
 function goodPractices() {
   let game = gameObject();
   for (let gameKey in game) {
     // are you ABSOLUTELY SURE what 'gameKey' is?
-    // use the debugger to find out!
-    debugger
+    // use the //debugger to find out!
+    //debugger
     let teamObj = game[gameKey]
     for (let teamKey in teamObj) {
       // are you ABSOLUTELY SURE what 'teamKey' is?
-      // use debugger to find out!
-      debugger
+      // use //debugger to find out!
+      //debugger
 
       // what is 'data' at each loop through out this block?
       // when will the following line of code work and when will it break?
       let data = teamObj.player
       for (let key in data) {
-        debugger
+        //debugger
       }
     }
   }
